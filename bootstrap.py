2e5b7ac87a5ab442c6fceca06ba79fdc40e0b180
cleanlines <fraser.hand@gmail.com>
2019-07-23 09:23:56 +1200

    Release 2

--- bootstrap.py
+++ bootstrap.py
@@ -8,9 +8,7 @@ from Decorator import Decorator
 class UpdateViewWrapper(BaseObject):
     def run_processes(self):
         try:
-            #[ProcessFactory.create_process(p).run_process() for p in [ProcessEnum.CORE_SERVICE, ProcessEnum.SECURITY_GROUPS, ProcessEnum.VIEWS, , ProcessEnum.CONTEXT_SERVICES, ProcessEnum.TEMPFILES]]
-            ProcessFactory.create_process(ProcessEnum.SECURITY_GROUPS).run_process()
-            ProcessFactory.create_process(ProcessEnum.CONTEXT_SERVICES).run_process()
+            [ProcessFactory.create_process(p).run_process() for p in [ProcessEnum.CORE_SERVICE, ProcessEnum.SECURITY_GROUPS, ProcessEnum.VIEWS, ProcessEnum.CONTEXT_SERVICES, ProcessEnum.TEMPFILES]]
         except Exception as e:
             self.errorlog(str(e))
 
